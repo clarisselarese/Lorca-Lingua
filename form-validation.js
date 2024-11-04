b39f837a2b9b1e80b2b631222082899085df784a
clarisse.larese <clarisse.larese@yahoo.fr>
2024-11-04 13:47:53 +0100

    Name, surname and email valid

--- form-validation.js
+++ form-validation.js
@@ -13,34 +13,52 @@ form.addEventListener("submit", (e) => {
   }
 });
 
+function validate() {
+  const isNameValid = validateNameAndSurname("name");
+  const isSurnameValid = validateNameAndSurname("surname");
+  const isEmailValid = validateEmail();
 
-
-
-function validate () {
-  const isNameValid = validateNameAndLastName("name")
-  const isSurnameValid = validateNameAndLastName("surname")
-
- return isNameValid && isSurnameValid
+  return isNameValid && isSurnameValid && isEmailValid;
 }
 
-function validateNameAndLastName(nameOrSurname) {
+function validateNameAndSurname(nameOrSurname) {
   const inputElement = document.getElementById(nameOrSurname);
   const inputValue = inputElement.value;
 
-  const errorID = nameOrSurname + 'Error'
-  const surnameError = document.getElementById(errorID); 
+  const errorID = nameOrSurname + "Error";
+
+  const surnameError = document.getElementById(errorID);
   surnameError.textContent = "";
 
   if (inputValue === null || inputValue === "" || inputValue.length === 1) {
-    
-    
     surnameError.textContent = errorMessages.nameOrSurname;
     return false;
+  } else {
+    return true;
+  }
+}
+
+function validateEmail () {
+  const inputEmail = document.getElementById("email");
+  const emailValue = inputEmail.value;
+
+  const emailError = document.getElementById("emailError");
+  emailError.textContent = "";
+
+  if (emailValue === null || emailValue === "") {
+    emailError.textContent = errorMessages.emailRequired;
+    return false;
+  } 
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const isRegexValid = emailRegex.test(emailValue)
+
+  if (!isRegexValid) {
+    emailError.textContent = errorMessages.invalidEmail;
+    return false
   }
 
-  return true 
-};
-
-
-
+  return true
+  
+}
 
