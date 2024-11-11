8df69b2926d9847bed4205a4c111a4dd556c729d
clarisse.larese <clarisse.larese@yahoo.fr>
2024-11-11 14:42:06 +0100

    email contact form done

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
-const express = require("express");
-const multer = require("multer");
-const path = require("path");
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const upload = multer({ dest: 'uploads/' });
 
-
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.filename + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-// setting file limits and validations
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 2 },
-  fileFilter: (req, file, cb) => {
-    const filetypes = "/pdf|doc|docx|jpg|jpeg|png";
-    const extname = filetypes.test(
-      path.extname(file.originalname).toLowerCase()
-    );
-
-    if (extname) {
-      return cb(null, true);
-    } else {
-      cb("Error: Only PDF, DOC, DOCX, JPG, JPEG, and PNG files are allowed");
-    }
-  },
-});
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// single file upload
-app.post("/upload", upload.single("file"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send(`No file uploaded.`);
-  }
-  res.send(`file uploaded: ${req.file.filename}`);
+app.post('/send-email', upload.single('file'), (req, res) => { 
+  console.log(req.body); 
+  console.log(req.file); 
+  // Utilisation d'un fichier pour simplifier
+  const { surname, name, enterprise, email, phoneNumber, urgent, message } = req.body;
+  const file = req.file; // Utilise `req.file` au lieu de `req.files` pour un fichier unique
+
+
+     // Create a nodemailer transporter
+     const transporter = nodemailer.createTransport({
+      service: 'Gmail', // or use your email provider
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,  
+      }
+  });
+
+   // Setup email data with unicode symbols
+      const mailOptions = {
+          from: `${name} <${process.env.EMAIL_USER}>`,
+          to: process.env.MAIL_USER,
+          subject: "Demande de devis" + " From:" + email,
+          text: ` 
+          Surname: ${surname}
+          Name: ${name}
+          Entreprise: ${enterprise}
+          Email: ${email}
+          Phone number ${phoneNumber}
+          Urgent: ${urgent}
+
+          Message:
+          ${message}
+          `,
+          attachments: [
+              {
+                  filename: file.originalname,
+                  path: file.path
+              }
+          ]
+      };
+
+      // Send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return res.status(500).send(error.toString());
+          }
+          fs.unlinkSync(file.path);
+          res.send('<script>alert("Email sent successfully!"); window.location.href = "/";</script>');
+      });
+  
 });
 
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
