881b5b40fd490e927411db60c8fb76c9102e26eb
clarisse.larese <clarisse.larese@yahoo.fr>
2024-12-02 12:42:44 +0100

    sticky navbar done

---
+++ JS/sticky-navbar.js
@@ -0,0 +1,15 @@
+let lastScroll = 0;
+
+let headerSticky = document.getElementById("header-sticky");
+
+window.addEventListener("scroll", (e) => {
+  console.log(e);
+
+  if (window.scrollY < lastScroll) {
+    headerSticky.style.top = 0;
+  } else {
+    headerSticky.style.top = "-36px";
+    headerSticky.style.boxShadow = "0px 5px 40px rgba(0, 0, 0, 0.1)";
+  }
+  lastScroll = window.scrollY;
+});
