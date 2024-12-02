738a315fca68deee943806c757c5fdc7c3ba182f
clarisse.larese <clarisse.larese@yahoo.fr>
2024-12-02 14:32:21 +0100

    shadow from sticky navbar removed in obile

--- JS/sticky-navbar.js
+++ JS/sticky-navbar.js
@@ -2,14 +2,15 @@ let lastScroll = 0;
 
 let headerSticky = document.getElementById("header-sticky");
 
-window.addEventListener("scroll", (e) => {
-  console.log(e);
+window.addEventListener("scroll", () => {
 
   if (window.scrollY < lastScroll) {
     headerSticky.style.top = 0;
-  } else {
+  } else if (window.innerWidth > 920) {
+
     headerSticky.style.top = "-6vh";
     headerSticky.style.boxShadow = "0px 5px 40px rgba(0, 0, 0, 0.1)";
   }
   lastScroll = window.scrollY;
 });
+
